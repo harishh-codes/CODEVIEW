42938284d4486ae4e9c47cc454cd16883b9cf981
Harishh Codes <harish.m.choudhary12@gmail.com>
2024-09-27 11:52:16 +0530

    Added all Files

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public/css")));
+app.use(express.static(path.join(__dirname,"public/js")));
+
+app.get("/",(req,res) => {
+    res.render("home.ejs");
+});
+
+app.get("/login",(req,res) => {
+    res.render("login.ejs");
+});
+
+app.get("/signup",(req,res)=> {
+    res.render("signup.ejs");
+});
+
+app.get("/about",(req,res)=> {
+    res.render("about.ejs");
+});
+
+app.get("/resources",(req,res) => {
+    res.render("resources.ejs");
+});
+
+app.get("/topic",(req,res) => {
+    res.render("topic.ejs");
+});
+
+app.listen(port,()=> {
+    console.log(`Listening on port ${port}`);
+});
